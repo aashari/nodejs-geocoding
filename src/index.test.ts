18f4f2fa8ace1603832299cb6825dd3a7adc53c9
aashari <andi@ashari.me>
2022-04-25 22:34:23 +0000

    add jest for unit testing

---
+++ src/index.test.ts
@@ -0,0 +1,27 @@
+import { encode, decode } from '../src/index';
+
+describe('testing index.ts', () => {
+
+    test('encode should return attributes: latitude, longitude, and formatted_address', async () => {
+        expect(await encode('Jalan Medan Merdeka Utara No.3, RT.3/RW.2')).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    latitude: expect.any(Number),
+                    longitude: expect.any(Number),
+                    formatted_address: expect.any(String),
+                }),
+            ])
+        )
+    });
+
+    test('decode should return attributes: latitude, longitude, and formatted_address', async () => {
+        expect(await decode(-6.170131, 106.8241607)).toEqual(
+            expect.objectContaining({
+                latitude: expect.any(Number),
+                longitude: expect.any(Number),
+                formatted_address: expect.any(String),
+            }),
+        )
+    });
+
+});
